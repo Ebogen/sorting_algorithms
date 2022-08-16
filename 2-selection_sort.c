2d94aaf5868174399b7c24f711db4977b3b986d0
Ebogen <ebogensamuel@gmail.com>
2022-08-16 13:47:00 -0700

    Task 2 Selection Sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - represents function to sort an array using
+ *                  selection sort algorithm
+ *
+ * @array: represents the array to be sorted
+ * @size: represents the size of array to be sorted
+ *
+ * Return: void
+ */
+
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, position;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		position = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[position] > array[j])
+				position = j;
+		}
+		if (position != i)
+		{
+			temp = array[i];
+			array[i] = array[position];
+			array[position] = temp;
+			print_array(array, size);
+		}
+	}
+}
